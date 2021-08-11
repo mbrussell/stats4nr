3d370bd8731c69ccd8d099ce908fcaa5bda69ca2
mbrussell <russellm@umn.edu>
2021-08-11 13:55:09 -0500

    Added folders.

---
+++ R/csv_to_rda.R
@@ -0,0 +1,23 @@
+# Load data sets as .csvs and publish as .rda files
+library(tidyverse)
+
+# elm
+elm <- read_csv("C://Users//russellm//Documents//Classes//UMN//NR 5021//Data//elm.csv")
+
+usethis::use_data(elm)
+
+# iron
+iron <- read_csv("C://Users//russellm//Documents//Classes//UMN//NR 5021//Data//iron.csv")
+
+usethis::use_data(iron)
+
+# birds
+birds <- read_csv("C://Users//russellm//Documents//Classes//UMN//NR 5021//Data//birds.csv")
+
+usethis::use_data(birds)
+
+# ant
+ant <- read_csv("C://Users//russellm//Documents//Projects//SNRAwR//SNRAwR//Data//ant.csv")
+
+usethis::use_data(ant)
+
